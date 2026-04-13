a41fd8ff30be106324b9e90690586992f9e49de9
RebeloDEV <carloseduardorebelo03@gmail.com>
2026-04-13 14:44:08 -0300

    Add função com estrutura de decisão.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
-function saldoVitorias(winsPlayer,lossPlayer){
+function saldo(winsPlayer,lossPlayer){
     let calc = winsPlayer-lossPlayer
     return calc
 }
 
-console.log ("O Herói tem de saldo de " + saldoVitorias(86,7))
+function nivelHeroi(saldo){
+
+    if (saldo <= 10){
+        return 'Ferro'
+    }
+
+    else if (saldo >= 11 && saldo <= 20){
+        return 'Bronze'
+    }
+
+    else if (saldo >= 21 && saldo <= 50){
+        return 'Prata'
+    }
+
+    else if (saldo >= 51 && saldo <= 80){
+        return 'Ouro'
+    }
+
+    else if (saldo >= 81 && saldo <= 90){
+        return 'Diamante'
+    }
+
+    else if (saldo >= 91 && saldo <= 100){
+        return 'Lendário'
+    }
+
+    else{
+        return 'Imortal'
+    }
+
+}
+
+let saldoVitorias = saldo
+let nivel = nivelHeroi
+
+console.log('O Herói tem de saldo de '+ saldoVitorias + " está no nível de " + nivel)
